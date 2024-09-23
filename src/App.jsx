0f2514fffba95144a898fe1110a03d3c75828585
Vanessa Kasminoff <sunnset07@gmail.com>
2024-09-23 00:05:43 -0700

    Applied css to hero

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,30 @@ function App() {
   return (
     <div id="App">
       <div className="hero-section">
-        <div className="hero-text">
-          <h1 className="heading-primary">
-            A healthy meal delivered to your door, every single day
-          </h1>
-          <p className="hero-description">
-            The smart 365-days-per-year food subscription that will make you eat
-            healthy again. Tailored to your personal tastes and nutritional
-            needs. We have delivered 250,000+ meals last year!
-          </p>
-          <a href="#" className="btn">Start eating well</a>
-          <a href="#" className="btn">Learn more &darr;</a>
-        </div>
-        <div className="hero-img">
-          <img src={heroImg} alt="Delicious meals and a woman eating pasta" />
+        <div className="hero">
+          <div className="hero-text">
+            <h1 className="heading-primary">
+              A healthy meal delivered to your door, every single day
+            </h1>
+            <p className="hero-description">
+              The smart 365-days-per-year food subscription that will make you
+              eat healthy again. Tailored to your personal tastes and
+              nutritional needs. We have delivered 250,000+ meals last year!
+            </p>
+            <a href="#" className="btn btn-full margin-right-sm">
+              Start eating well
+            </a>
+            <a href="#" className="btn btn-outline">
+              Learn more &darr;
+            </a>
+          </div>
+          <div className="hero-img-section">
+            <img
+              className="hero-img"
+              src={heroImg}
+              alt="Food in bowls on a table, meals in containers, and a woman eating pasta"
+            />
+          </div>
         </div>
       </div>
     </div>
