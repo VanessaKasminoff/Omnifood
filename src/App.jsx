cb56702aa8732641aed9d2de19b63f9ff74ad4cd
Vanessa Kasminoff <sunnset07@gmail.com>
2024-09-22 23:26:03 -0700

    Implemented hero content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
-import './assets/css/App.css'
+import "./assets/css/App.css";
+import heroImg from "./assets/img/hero.png";
 
 function App() {
-
   return (
-    <>
-    <h1>A healthy meal delivered to your door, every single day</h1>
-    </>
-  )
+    <div id="App">
+      <div className="hero-section">
+        <div className="hero-text">
+          <h1 className="heading-primary">
+            A healthy meal delivered to your door, every single day
+          </h1>
+          <p className="hero-description">
+            The smart 365-days-per-year food subscription that will make you eat
+            healthy again. Tailored to your personal tastes and nutritional
+            needs. We have delivered 250,000+ meals last year!
+          </p>
+          <a href="#" className="btn">Start eating well</a>
+          <a href="#" className="btn">Learn more &darr;</a>
+        </div>
+        <div className="hero-img">
+          <img src={heroImg} alt="Delicious meals and a woman eating pasta" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+/*
+- Logo + Navigation
+- Hero
+- Featured in
+- How it works
+- Diets
+- Meals (and list of diets)
+- Testimonials + gallery
+- Pricing + features
+- CTA
+- Footer
+*/
