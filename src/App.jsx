8b42f9dd8599460ca954394075dbc7255fbfb42e
Vanessa Kasminoff <sunnset07@gmail.com>
2024-10-23 17:53:11 -0700

    Refactored code to move the form into its own component

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,7 @@ import Testimonial from "./components/Testimonial";
 import GalleryPhoto from "./components/GalleryPhoto";
 import PricingCard from "./components/PricingCard";
 import FeatureCard from "./components/FeatureCard";
+import Form from "./components/Form";
 import Footer from "./components/Footer";
 
 function App() {
@@ -358,65 +359,9 @@ function App() {
             />
           </div>
         </div>
-
-        <div className="cta-section">
-          <div className="container">
-            <div className="cta">
-              <div className="cta-textbox">
-                <h2 className="heading-secondary">
-                  Get your first meal for free!
-                </h2>
-                <p className="cta-text">
-                  Healthy, tasty and hassle-free meals are waiting for you.
-                  Start eating well today. You can cancel or pause anytime. And
-                  the first meal is on us!
-                </p>
-                <form className="cta-form" action="#">
-                  <div>
-                    <label for="full-name">Full Name*</label>
-                    <input
-                      id="full-name"
-                      placeholder="John Smith"
-                      type="text"
-                      required
-                    />
-                  </div>
-                  <div>
-                    <label for="email">Email Address*</label>
-                    <input
-                      id="email"
-                      placeholder="example@example.com"
-                      type="email"
-                      required
-                    />
-                  </div>
-                  <div>
-                    <label for="select-where">
-                      Where did you hear about us?*
-                    </label>
-                    <select id="select-where" required>
-                      <option value="">--Please choose an option--</option>
-                      <option value="friends">Friends and family</option>
-                      <option value="youtube">YouTube</option>
-                      <option value="podcast">Podcast</option>
-                      <option value="tiktok">TikTok</option>
-                      <option value="other">Other</option>
-                    </select>
-                  </div>
-                  <button className="btn btn--form">Sign up now</button>
-                </form>
-              </div>
-              <div
-                className="cta-img-box"
-                role="img"
-                aria-label="Woman enjoying food"
-              >
-              </div>
-            </div>
-          </div>
-        </div>
+        <Form />
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
